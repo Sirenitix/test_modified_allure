a4dabfb08ac41a2b4cfe74fb6f232f3b460f75ee
rualdinuly <ramazan.ualdinuly@alumni.nu.edu.kz>
2022-09-05 18:07:07 +0600

    18.07

--- src/test/java/com/example/catalog_service_api_tests/Offers.java
+++ src/test/java/com/example/catalog_service_api_tests/Offers.java
@@ -6,8 +6,10 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.hamcrest.core.Is.is;
+
 
 public class Offers {
     @BeforeAll
@@ -19,6 +21,6 @@ public class Offers {
     @DisplayName("Returns the list of offers by product id")
     public void getListOfOffersByProductId(){
         RequestSpecification requestSpecification = RestAssured.given();
-        requestSpecification.given().get("https://test4.jmart.kz/gw/catalog/v1/products/offers/100000").then().assertThat().body("success", is())
+        requestSpecification.given().get("https://test4.jmart.kz/gw/catalog/v1/products/offers/1").then().assertThat().body("success", is(true));
     }
 }
