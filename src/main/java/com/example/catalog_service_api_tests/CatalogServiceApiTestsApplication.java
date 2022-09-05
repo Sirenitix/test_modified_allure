982a9072f38d30d74f37bf1c8d8a6eadeb58f0a2
nsuleyev <suleevn@gmail.com>
2022-09-05 17:18:05 +0600

    Initial commit

---
+++ src/main/java/com/example/catalog_service_api_tests/CatalogServiceApiTestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.catalog_service_api_tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogServiceApiTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogServiceApiTestsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/catalog_service_api_tests/CatalogServiceApiTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.catalog_service_api_tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApiTestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
