f637a87724916e8716f53a05989a1bbdba173188
rualdinuly <ramazan.ualdinuly@alumni.nu.edu.kz>
2022-09-05 17:56:27 +0600

    17:56

---
+++ src/test/java/com/example/catalog_service_api_tests/Offers.java
@@ -0,0 +1,24 @@
+package com.example.catalog_service_api_tests;
+
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static io.restassured.RestAssured.given;
+
+public class Offers {
+    @BeforeAll
+    public static void setup(){
+        RestAssured.baseURI="https://test4.jmart.kz/";
+    }
+    @Test
+    @Order(1)
+    @DisplayName("Returns the list of offers by product id")
+    public void getListOfOffersByProductId(){
+        RequestSpecification requestSpecification = RestAssured.given();
+        requestSpecification.given().get("https://test4.jmart.kz/gw/catalog/v1/products/offers/100000").then().assertThat().body("success")
+    }
+}
