674bb489ff1854519152dfeddcf32f953efac464
nsuleyev <suleevn@gmail.com>
2022-09-06 12:26:37 +0600

    12:26

---
+++ src/test/java/com/example/catalog_service_api_tests/AbstractTest.java
@@ -0,0 +1,47 @@
+package com.example.catalog_service_api_tests;
+
+import com.example.catalog_service_api_tests.entity.Configurations;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static io.restassured.RestAssured.given;
+
+@Slf4j
+@SpringBootTest
+public abstract class AbstractTest {
+
+    @Autowired
+    private Configurations configurations;
+    static RequestSpecification requestSpecification;
+
+
+    @BeforeEach
+    protected void setup(){
+        RestAssured.baseURI = configurations.getBaseUri();
+        Response response = given()
+                .params("login", configurations.getLogin(), "password", configurations.getPassword())
+                .post(configurations.getSignIn())
+                .then()
+                .log()
+                .body()
+                .statusCode(201)
+                .extract()
+                .response();
+        String access_token = response.path("data.tokens.auth.token").toString();
+        log.info(access_token + " - access_token");
+        String refresh_token = response.path("data.tokens.refresh.token").toString();
+        String Authorization = "Bearer " + access_token;
+        RequestSpecBuilder builder = new RequestSpecBuilder();
+        builder.addHeader("Authorization", Authorization);
+        builder.addHeader("Refresh_token", refresh_token);
+        builder.addHeader("Content-Type", "application/json");
+        requestSpecification = builder.build();
+
+    }
+}
--- src/test/java/com/example/catalog_service_api_tests/СategoryСontentTemplates.java
+++ src/test/java/com/example/catalog_service_api_tests/СategoryСontentTemplates.java
@@ -7,6 +7,7 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,39 +19,16 @@ import static org.hamcrest.Matchers.*;
 
 
 @Slf4j
-@SpringBootTest
-public class СategoryСontentTemplates {
+public class СategoryСontentTemplates extends AbstractTest {
+
+    @Override
+    @BeforeEach
+    public void setup() {
+        super.setup();
+    }
 
     @Autowired
     private Configurations configurations;
-
-    private static RequestSpecification requestSpecification;
-
-
-    @BeforeEach
-    public void setup(){
-        RestAssured.baseURI = configurations.getBaseUri();
-        Response response = given()
-                .params("login", configurations.getLogin(), "password", configurations.getPassword())
-                .post(configurations.getSignIn())
-                .then()
-                .log()
-                .body()
-                .statusCode(201)
-                .extract()
-                .response();
-        String access_token = response.path("data.tokens.auth.token").toString();
-        log.info(access_token + " - access_token");
-        String refresh_token = response.path("data.tokens.refresh.token").toString();
-        String Authorization = "Bearer " + access_token;
-        RequestSpecBuilder builder = new RequestSpecBuilder();
-        builder.addHeader("Authorization", Authorization);
-        builder.addHeader("Refresh_token", refresh_token);
-        builder.addHeader("Content-Type", "application/json");
-        requestSpecification = builder.build();
-
-    }
-
 
     @Test
     @DisplayName("get feature handbook test")
@@ -58,7 +36,7 @@ public class СategoryСontentTemplates {
         given()
                 .log().all()
                 .when()
-                .spec(requestSpecification)
+                .spec(AbstractTest.requestSpecification)
                 .get(configurations.getFeatureHandbookPath())
                 .then()
                 .assertThat()
