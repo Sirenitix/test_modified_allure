f6bc1d38dd9717766ba4dfd354229e6452b75767
rualdinuly <ramazan.ualdinuly@alumni.nu.edu.kz>
2022-09-07 09:08:59 +0600

    9:08

--- src/test/java/com/example/catalog_service_api_tests/CategoryContentTemplateProduct.java
+++ src/test/java/com/example/catalog_service_api_tests/CategoryContentTemplateProduct.java
@@ -197,20 +197,5 @@ public class CategoryContentTemplateProduct {
                 .body("status", is("A"));
         //This API request is done wrong. It does not searches for the product. Error 404 returns
     }
-    @Test
-    @Order(12)
-    @DisplayName("Change the status of a product")
-    public void putChangeTheStatusOfProduct(){
-        String status = "A";
-        String product_code = "667wws667wws";
-        given()
-                .when()
-                .spec(requestSpecification)
-                .body("{ \"status\": \"" + status + "\"}")
-                .put("https://test4.jmart.kz/gw/catalog/v1/category-content-template-product/status-change/1670296")
-                .then()
-                .assertThat()
-                .body("status", is("A"));
-        //This API request is done wrong. It does not searches for the product. Error 404 returns
-    }
+
 }
