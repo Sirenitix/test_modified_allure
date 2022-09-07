072c5237be86154886350bf904047e071c453e1b
mazhikenalikhan <mazhiken.alikhan@gmail.com>
2022-09-07 17:56:02 +0600

    UPDATED

--- src/test/java/com/example/catalog_service_api_tests/CatalogV2.java
+++ src/test/java/com/example/catalog_service_api_tests/CatalogV2.java
@@ -11,12 +11,88 @@ public class CatalogV2 extends AbstractTest{
                 .when()
                 .body("{\n" +
                         "  \"ids\": [\n" +
-                        "    0\n" +
+                        "    dfvdfdg\n" +
                         "  ]\n" +
                         "}")
-                .post("/gw/catalog/v2/products/by_ids")
+                .contentType("application/json")
+                .post("https://test4.jmart.kz/gw/catalog/v2/products/by_ids")
+                .then()
+                .assertThat()
+                .statusCode(422);
+        given()
+                .when()
+                .body("{\n" +
+                        "  \"ids\": [\n" +
+                        "    1.5\n" +
+                        "  ]\n" +
+                        "}")
+                .contentType("application/json")
+                .post("https://test4.jmart.kz/gw/catalog/v2/products/by_ids")
+                .then()
+                .assertThat()
+                .statusCode(422);
+        given()
+                .when()
+                .body("{\n" +
+                        "  \"ids\": [\n" +
+                        "    1\n" +
+                        "  ]\n" +
+                        "}")
+                .contentType("application/json")
+                .post("https://test4.jmart.kz/gw/catalog/v2/products/by_ids")
                 .then()
                 .assertThat()
                 .statusCode(200);
+        given()
+                .when()
+                .contentType("application/json")
+                .post("https://test4.jmart.kz/gw/catalog/v2/products/by_ids")
+                .then()
+                .assertThat()
+                .statusCode(422);
+        given()
+                .when()
+                .body("{\n" +
+                        "  \"ids\": [\n" +
+                        "  ]\n" +
+                        "}")
+                .contentType("application/json")
+                .post("https://test4.jmart.kz/gw/catalog/v2/products/by_ids")
+                .then()
+                .assertThat()
+                .statusCode(422);
+        given()
+                .when()
+                .body("{\n" +
+                        "}")
+                .contentType("application/json")
+                .post("https://test4.jmart.kz/gw/catalog/v2/products/by_ids")
+                .then()
+                .assertThat()
+                .statusCode(422);
+        given()
+                .when()
+                .body("{\n" +
+                        "  \"ids\": [\n" +
+                        "    'adf'\n" +
+                        "  ]\n" +
+                        "}")
+                .contentType("application/json")
+                .post("https://test4.jmart.kz/gw/catalog/v2/products/by_ids")
+                .then()
+                .assertThat()
+                .statusCode(422);
+        given()
+                .when()
+                .body("{\n" +
+                        "  \"ids\": [\n" +
+                        "    -1\n" +
+                        "  ]\n" +
+                        "}")
+                .contentType("application/json")
+                .post("https://test4.jmart.kz/gw/catalog/v2/products/by_ids")
+                .then()
+                .assertThat()
+                .statusCode(422);
     }
 }
